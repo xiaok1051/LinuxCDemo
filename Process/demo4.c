055e71d5a52b1aecda31b1a077cc4468da5cb537
xiaok1051 <xiaok1051@163.com>
2018-07-15 22:21:06 +0800

    wait child process exit

--- Process/demo4.c
+++ Process/demo4.c
@@ -31,7 +31,31 @@ int main(int argc, char *argv[])
 		exit(7); /*子进程调用exit函数，参数为7*/
 	}
 
+	if(wait(&status) != pid)
+	{
+		perror("fork error");
+		exit(-1);
+	}
+	print_exit_status(status); /*打印退出转台信息*/
 
 
+	/*创建第二个子进程*/
+	if((pid = fork()) < 0)
+	{
+		perror("fork error");
+		exit(-1);
+	}
+	else if(pid == 0)
+	{
+		abort(); /*子进程调用abort()函数异常退出*/
+	}
 
+	if(wait(&status) != pid) /*父进程等待子进程退出，并获取退出状态*/
+	{
+		perror("fork error");
+		exit(-1);
+	}
+	print_exit_status(status); /*打印第二个退出状态信息*/
+	
+	return 0;
 }
